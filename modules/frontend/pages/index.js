2e2cb495c3fc858a1af2af6ab3e6549039548ba3
tanner_emerson <tanner.p.emerson@gmail.com>
2021-12-28 19:20:10 -0600

    [frontend] convert a tags to Links

--- modules/frontend/pages/index.js
+++ modules/frontend/pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Link from "next/head";
+import Link from "next/link";
 import Footer from "../components/footer";
 import Header from "../components/header";
 import styles from "../styles/Home.module.css";
@@ -16,40 +16,51 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>Hi, I'm Tanner!</h1>
         <div className={styles.grid}>
-          <a href="/about" className={styles.card}>
-            <h3>About &rarr;</h3>
-            <p>
-              Learn about the technologies and decisions powering this site.
-            </p>
-          </a>
+          <Link href="/about">
+            <a className={styles.card}>
+              <h3>About &rarr;</h3>
+              <p>
+                Learn about the technologies and decisions powering this site.
+              </p>
+            </a>
+          </Link>
 
-          <a href="/running" className={styles.card}>
-            <h3>Running &rarr;</h3>
-            <p>
-              Read about my running journey and check out my stats. I've got big
-              goals for 2021!
-            </p>
-          </a>
+          <Link href="/running">
+            <a className={styles.card}>
+              <h3>Running &rarr;</h3>
+              <p>
+                Read about my running journey and check out my stats. I've got big
+                goals for 2021!
+              </p>
+            </a>
+          </Link>
 
-          <a href="/programming" className={styles.card}>
-            <h3>Programming &rarr;</h3>
-            <p>These are the projects I want to share with you : ).</p>
-          </a>
+          <Link href="/programming">
+            <a className={styles.card}>
+              <h3>Programming &rarr;</h3>
+              <p>These are the projects I want to share with you : ).</p>
+            </a>
+          </Link>
 
-          <a href="/interests" className={styles.card}>
-            <h3>Interests &rarr;</h3>
-            <p>
-              Videos that I felt had learning with sharing, hope you enjoy too.
-            </p>
-          </a>
+          <Link href="/interests">
+            <a className={styles.card}>
+              <h3>Interests &rarr;</h3>
+              <p>
+                Videos that I felt had learning with sharing, hope you enjoy too.
+              </p>
+            </a>
+          </Link>
 
-          <a href="/goals" className={styles.card}>
-            <h3>Goals &rarr;</h3>
-            <p>Interested in what i'm trying to achieve?</p>
-          </a>
+          <Link href="/goals">
+            <a className={styles.card}>
+              <h3>Goals &rarr;</h3>
+              <p>Interested in what i'm trying to achieve?</p>
+            </a>
+          </Link>
+
         </div>
       </main>
       <Footer />
     </div>
-  );
+);
 }
