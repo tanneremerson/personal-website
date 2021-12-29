46ddd4b7dc7fedf78663406a5f0d67067466daaf
tanner_emerson <tanner.p.emerson@gmail.com>
2021-12-28 23:05:02 -0600

    Comment out database connection

--- modules/frontend/pages/api/hello.js
+++ modules/frontend/pages/api/hello.js
@@ -1,26 +1,26 @@
-import knex from 'knex'
-
-const connection = knex({
-  client: 'mysql',
-  asyncStackTraces: true,
-  debug: true,
-  connection: {
-    database: process.env.DATABASE,
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    ssl: true
-  }
-});
+// import knex from 'knex'
+//
+// const connection = knex({
+//   client: 'mysql',
+//   asyncStackTraces: true,
+//   debug: true,
+//   connection: {
+//     database: process.env.DATABASE,
+//     host: process.env.HOST,
+//     user: process.env.USER,
+//     password: process.env.PASSWORD,
+//     ssl: true
+//   }
+// });
 
 
 export default async function handler(_, res) {
-  try {
-    const r = await connection.select('1+1').as('test')
-    console.log({ r })
-  } catch (e) {
-    console.log({e})
-  }
+  // try {
+  //   const r = await connection.select('1+1').as('test')
+  //   console.log({ r })
+  // } catch (e) {
+  //   console.log({e})
+  // }
 
   res.status(200).json({ name: 'John Doe' })
 }
